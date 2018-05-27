d6152a9aaad62e5a0e5c77abbaee799c3f687e2c
Elvendir <39671719+Elvendir@users.noreply.github.com>
2018-05-27 19:01:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import codecs   
+import glob
+import time
+from main_functions import * 
+from calc_stamina import *
+from map_extraction import *
+
+
+g = codecs.open('DATAs','w','utf-8')
+
+folder_path = "" #path of floder with .osu files
+
+songs_new_difficulty = []
+songs_osu_difficulty = []
+
+
+for element in os.listdir(folder_path):
+
+	file_path = folder_path+element
+
+	(osu_difficulty,map,nb_columns)=extract_info(file_path)
+	
+	(stamina,kps_columns,i_columns) = calc_stamina(map,nb_columns)
+	
+	kps=calc_kps(kps_columns,i_columns,map)
+	overall_difficulty = calc_overall_difficulty(np.array(stamina),np.array(kps))
+	g.write(name';'overall_difficulty)
+	songs_osu_difficulty.append(osu_difficulty)
+	songs_new_difficulty.append(overall_difficulty)
+	
+plt.plot(songs_osu_difficulty,songs_new_difficulty,'+')
+g.close()
+
+
+
+
---
+++ map_extraction.py
@@ -0,0 +1,42 @@
+import numpy as np
+import os
+import sys
+import codecs   
+import glob
+
+def extract_info(file):
+	file = os.path.abspath(file) 
+	f = codecs.open(file, 'r', 'utf-8')
+	rd =f.readline()
+	b1 = False
+	map = []
+	columns = []
+	i = 0
+	osu_difficulty = float(rd)
+	while rd:
+		b2 = False
+		if b1:
+			object  = np.array(rd.split(','))
+			for column in columns :
+				if column == int(object[0]):
+					b2 = True
+			if not(b2) :
+				columns.append(int(object[0])) 
+			if object[3] == 128 :
+				map.append(np.array([int(object[0]),1,int(object[2])]))
+				map.append(np.array([int(object[0]),2,int(object[5])]))
+			else :
+				map.append(np.array([int(object[0]),0,int(object[2])]))
+		if rd.count('HitObjects')==1:
+			b1 = True
+		rd = f.readline()
+	columns.sort()
+	for object in map :
+		i = 0
+		for column in columns :
+			if column == object[0]: 
+				object[0] = i
+			i += 1
+	f.close()
+	map = np.array(map)
+	return (osu_difficulty,map,len(columns))
