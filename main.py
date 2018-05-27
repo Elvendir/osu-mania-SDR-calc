10d066dbf218b1393fcb017fb7f850d3c7d1cf4a
Elvendir <elvendir.owo@gmail.com>
2018-05-27 19:18:42 +0200

    at least working now

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ from main_functions import *
 from calc_stamina import *
 from map_extraction import *
 
-
+print('Folder with only .osu files')
+folder_path = input()
 g = codecs.open('DATAs','w','utf-8')
-
-folder_path = "" #path of floder with .osu files
 
 songs_new_difficulty = []
 songs_osu_difficulty = []
@@ -20,18 +19,17 @@ songs_osu_difficulty = []
 
 for element in os.listdir(folder_path):
 
-	file_path = folder_path+element
+    file_path = folder_path+'/'+element
+    name = element
+    (map,nb_columns)=extract_info(file_path)
 
-	(osu_difficulty,map,nb_columns)=extract_info(file_path)
-	
-	(stamina,kps_columns,i_columns) = calc_stamina(map,nb_columns)
-	
-	kps=calc_kps(kps_columns,i_columns,map)
-	overall_difficulty = calc_overall_difficulty(np.array(stamina),np.array(kps))
-	g.write(name';'overall_difficulty)
-	songs_osu_difficulty.append(osu_difficulty)
-	songs_new_difficulty.append(overall_difficulty)
-	
+    (stamina,kps_columns,i_columns) = calc_stamina(map,nb_columns)
+
+    kps=calc_kps(kps_columns,i_columns,map)
+    overall_difficulty = calc_overall_difficulty(np.array(stamina),np.array(kps))
+    g.write(name+';'+ str(overall_difficulty)+'\n')
+    songs_new_difficulty.append(overall_difficulty)
+
 plt.plot(songs_osu_difficulty,songs_new_difficulty,'+')
 g.close()
 
--- stamina_functions.py
+++ stamina_functions.py
@@ -9,66 +9,72 @@ tau_s_memory = 20
 LN_release_kps_correction = .05
 LN_note_after_release_correction = .1
 
-def increment_i_column(i,i_columns,column) :
-	current_i_columns = [i_columns[i-1][k] for k in range(len(i_columns[i-1]))]
-	current_i_columns[column] = i
-	return(np.array(current_i_columns))
 
-def next_kps(i,i_columns,t,column,kps_columns,type) :
-	kps = [kps_columns[i-1][k] for k in range(len(kps_columns[i-1]))]
-	if i_columns[i-1][column] != -1 :
-		if type[i] == 2 :
-			Delta_t = t[i] - t[i_columns[i-1][column]] + LN_release_kps_correction
-			kps[column] =  1 / Delta_t
-		if type[i_columns[i-1][column]] == 2 :
-			Delta_t = max((t[i] - t[i_columns[i-1][column]] + LN_release_kps_correction, t[i] - t[i_columns[i_columns[i-1]][column]]))
-			kps[column] =  1 / Delta_t
-		else :
-			Delta_t = t[i] - t[i_columns[i-1][column]] 
-			kps[column] =  1 / Delta_t
-	return(np.array(kps))
-
-def G(kps,t) :
-	return 0.5*(pow(t/tau_target+1,kps**2/kps_target**2+1)-(t/tau_target+1))
-
-def d_G(kps,t) :
-	return (kps**2/kps_target**2+1)*(0.5/tau_target)*pow(t/tau_target+1,kps**2/kps_target**2)-0.5/tau_target
-
-def calc_target(kps,t,s,list_i,i) :
-
-	j = i
-	j_p1 = j
-	j_max = 0
-	kps_min = kps[i]
-	s_targeted = 0
-	s_targeted_max = 0
-	dg = 0
-	dg_max = 0
-	kps_mean = kps[i]
-
-	while list_i[j-1] > 0 :
-		Delta_t = t[j]-t[list_i[j-1]]
-		kps_mean = kps_mean + (kps[j] - kps_mean)*Delta_t/tau_kps_mean
-		if kps_mean <= kps_min :
-			kps_min = kps_mean
-		j_p1 = j
-		j = list_i[j-1]
-		s_targeted = G(kps_min,t[i]-t[j]) 
-		if s_targeted >= s_targeted_max :
-			s_targeted_max = s_targeted
-			j_max = j
-		dg = d_G(kps_min,t[i]-t[j])
-		if dg > dg_max :
-			dg_max = dg
-	
-	return (s_targeted_max,dg_max)
+def increment_i_column(i, i_columns, column):
+    current_i_columns = [i_columns[i - 1][k] for k in range(len(i_columns[i - 1]))]
+    current_i_columns[column] = i
+    return (np.array(current_i_columns))
 
 
-def dif_eq(kps,t,s,list_i,i):
-	i_m1 = list_i[i-1]
-	Delta_t = t[i]-t[i_m1]
-	(s_targeted, dg) = calc_target(kps,t,s,list_i,i)
-	if s[i_m1] > s_targeted :
-		return s[i_m1] + dg*Delta_t + (s_targeted - s[i_m1])*Delta_t/tau_higher
-	else:
-		return s[i_m1] + dg*Delta_t + (s_targeted - s[i_m1])*Delta_t/tau_lower 
+def next_kps(i, i_columns, t, column, kps_columns, type):
+    kps = [kps_columns[i - 1][k] for k in range(len(kps_columns[i - 1]))]
+    if i_columns[i - 1][column] != -1:
+        if type[i] == 2:
+            Delta_t = t[i] - t[i_columns[i - 1][column]] + LN_release_kps_correction
+            kps[column] = 1 / Delta_t
+        if type[i_columns[i - 1][column]] == 2:
+            Delta_t = max((t[i] - t[i_columns[i - 1][column]] + LN_release_kps_correction,
+                           t[i] - t[i_columns[i_columns[i - 1]][column]]))
+            kps[column] = 1 / Delta_t
+        else:
+            Delta_t = t[i] - t[i_columns[i - 1][column]]
+            kps[column] = 1 / Delta_t
+    return (np.array(kps))
+
+
+def G(kps, t):
+    return 0.5 * (pow(t / tau_target + 1, kps ** 2 / kps_target ** 2 + 1) - (t / tau_target + 1))
+
+
+def d_G(kps, t):
+    return (kps ** 2 / kps_target ** 2 + 1) * (0.5 / tau_target) * pow(t / tau_target + 1,
+                                                                       kps ** 2 / kps_target ** 2) - 0.5 / tau_target
+
+
+def calc_target(kps, t, s, list_i, i):
+    j = i
+    j_p1 = j
+    j_max = 0
+    kps_min = kps[i]
+    s_targeted = 0
+    s_targeted_max = 0
+    dg = 0
+    dg_max = 0
+    kps_mean = kps[i]
+
+    while list_i[j - 1] > 0:
+        Delta_t = t[j] - t[list_i[j - 1]]
+        kps_mean = kps_mean + (kps[j] - kps_mean) * Delta_t / tau_kps_mean
+        if kps_mean <= kps_min:
+            kps_min = kps_mean
+        j_p1 = j
+        j = list_i[j - 1]
+        s_targeted = G(kps_min, t[i] - t[j])
+        if s_targeted >= s_targeted_max:
+            s_targeted_max = s_targeted
+            j_max = j
+        dg = d_G(kps_min, t[i] - t[j])
+        if dg > dg_max:
+            dg_max = dg
+
+    return (s_targeted_max, dg_max)
+
+
+def dif_eq(kps, t, s, list_i, i):
+    i_m1 = list_i[i - 1]
+    Delta_t = t[i] - t[i_m1]
+    (s_targeted, dg) = calc_target(kps, t, s, list_i, i)
+    if s[i_m1] > s_targeted:
+        return s[i_m1] + dg * Delta_t + (s_targeted - s[i_m1]) * Delta_t / tau_higher
+    else:
+        return s[i_m1] + dg * Delta_t + (s_targeted - s[i_m1]) * Delta_t / tau_lower
