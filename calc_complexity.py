2d0d0d4e5243a52f2f32264e7c55846ebb59a8fe
Elvendir <elvendir.owo@gmail.com>
2018-12-28 13:44:36 +0100

    Faster FFT calc

--- calc_complexity.py
+++ calc_complexity.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 Reminder : type_note == 0 normal, 1 LN hold, 2 LN release
 '''
 
-TF_time_scale = 5
-sample_size = 400
+TF_time_scale = 1
+sample_size = 2000
+note_placement = 999
 space_btw_columns = 10
 
 def create_array(map,nb_columns):
@@ -18,9 +19,9 @@ def create_array(map,nb_columns):
         sample.append(np.array([0 for k in range(nb_columns+space_btw_columns*(nb_columns-1))]))
     i=0
     j=0
-    tc = t[0] + j * TF_time_scale
+    tc = t[0] + (j+note_placement) * TF_time_scale
     while t[i] <= tc :
-        sample[j][column[i]*space_btw_columns] = 1
+        sample[note_placement][column[i]*space_btw_columns] = 1
         i += 1
     j = 1
     return(sample,j,i)
@@ -28,38 +29,46 @@ def create_array(map,nb_columns):
 
 
 def increment_array(sample,j,i, map, nb_columns ):
-    sample.pop(0)
     t = map[:,2]
     column = map[:,0]
-    sample.append(np.array([0 for k in range(nb_columns + space_btw_columns * (nb_columns - 1))]))
-    tc = t[0] + j * TF_time_scale
-    while  i < len(map) and t[i] <= tc :
-        sample[sample_size-1][column[i]*space_btw_columns] = 1
-        i +=1
-    j += 1
-    return(sample,j,i)
+    nb_note =  0
+    tc = t[0] + (j+note_placement) * TF_time_scale
+    while t[i] > tc :
+        j += 1
+        sample.pop(0)
+        sample.append(np.array([0 for k in range(nb_columns + space_btw_columns * (nb_columns - 1))]))
+        tc += TF_time_scale
+
+    while i < len(map) and t[i] <= tc :
+        sample[note_placement][column[i] * space_btw_columns] = 1
+        nb_note += 1
+        i += 1
+    return(sample, j, i, nb_note )
 
 
 
 def calc_complexity(map, nb_columns):
-    (sample,j,i) = create_array(map, nb_columns)
-    a_sample = np.array(sample)
     complexity=[]
     t = map[:,2]
-    tc = t[0] + j * TF_time_scale
-    tt =[]
-    print(t[len(t)-1])
-    while tc < t[len(t)-1]+TF_time_scale*sample_size :
+
+
+
+    (sample,j,i) = create_array(map, nb_columns)
+    a_sample = np.array(sample)
+    fft_sample = abs(np.fft.rfft2(a_sample))
+    for k in range(i):
+        complexity.append(np.sum(fft_sample))
+
+    while i < len(map) :
+        (sample,j,i, nb_note)=increment_array(sample,j,i,map,nb_columns)
         a_sample = np.array(sample)
         fft_sample = abs(np.fft.rfft2(a_sample))
-        complexity.append(np.sum(fft_sample))
+        for k in range(nb_note):
+            complexity.append(np.sum(fft_sample))
         cmap = plt.get_cmap('PiYG')
         '''
         if j % 10000 == 0 :
             plt.pcolormesh(fft_sample)
             plt.show()
         '''
-        (sample,j,i)=increment_array(sample,j,i,map,nb_columns)
-        tt.append(tc - TF_time_scale * sample_size / 2)
-        tc = t[0] + j * TF_time_scale
-    return(complexity,tt)
+    return(complexity)
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ Write all song_name;difficulty in a DATA file
 g = codecs.open('DATAs', 'w', 'utf-8')
 print('Folder with only .osu files')
 folder_path = input()
-g.write('name;nb_column;true_nb_columns;dif \n')
+g.write('name;nb_column;true_nb_columns;kps;felt_kps;complexity;m;b;overall_dif \n')
 for element in os.listdir(folder_path):
     t0 = time.time()
     file_path = folder_path + '/' + element
@@ -28,33 +28,43 @@ for element in os.listdir(folder_path):
     (kps, left_i, right_i) = calc_kps(map, nb_columns)
 
     felt_kps = [0 for i in range(len(map))]
-
+    '''
     felt_kps = calc_felt_kps_stamina(map, left_i, kps, felt_kps)
     felt_kps = calc_felt_kps_stamina(map, right_i, kps, felt_kps)
+    '''
+    complexity = calc_complexity(map, nb_columns)
 
-    (complexity,tt) = calc_complexity(map, nb_columns)
-
-    mean_felt_kps = np.mean(np.array(felt_kps))
     mean_kps = np.mean(np.array(kps))
     mean_complexity = np.mean(np.array(complexity))
-    complexity2 = [0]
-    j = 0
-    for i in range(1, len(map)):
-        while tt[j] < map[i, 2]:
-            j += 1
-        complexity2.append(complexity[j])
 
-    overall_difficulty = np.mean(np.array(complexity2)*np.array(felt_kps))
+    overall_difficulty = np.mean(np.array(complexity)*np.array(kps))
+    (m,b) = np.polyfit(kps, complexity, 1)
+
     g.write(name)
     g.write(';' + str(nb_columns))
     g.write(';' + str(true_nb_columns))
     g.write(';' + str(mean_kps))
-    g.write(';' + str(mean_felt_kps))
+    g.write(';' + str(0))
     g.write(';' + str(mean_complexity))
-    g.write(';' + str(overall_difficulty))
-    print('dif = ' + str(overall_difficulty) + '; mean_complexity ='+ str(mean_complexity)  + '; mean_kps = ' + str(
-        mean_kps) + '; name = ' + name + '; nb_note = ' + str(
-        len(map)) + '; calc_time = ' + str(
-        time.time() - t0) + '; nb_columns = ' + str(nb_columns) + '; true_nb =' + str(true_nb_columns))
+    g.write(';' + str(m))
+    g.write(';' + str(b))
+    g.write(';' + str(overall_difficulty) + '\n' )
+    print('dif = ' + str("%.2f" % overall_difficulty)
+          + '; m_c ='+ str("%.2f" % mean_complexity)
+          + '; m =' + str("%.2f" % m)
+          + '; b =' + str("%.2f" % b)
+          + '; m_kps = ' + str("%.2f" % mean_kps)
+          + '; calc_t = ' + str("%.2f" % (time.time() - t0))
+          + '; name = ' + name
+          + '; nb_note = ' + str(len(map))
+          + '; nb_columns = ' + str(nb_columns)
+          + '; true_nb =' + str(true_nb_columns))
+
+'''
+    fit_fn = np.poly1d(np.array([m,b]))
+    plt.plot(kps,complexity,'.')
+    plt.plot(kps,fit_fn(kps))
+    plt.show()
+'''
 
 g.close()
